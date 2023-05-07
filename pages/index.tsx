f9ccbe6c926c68e7215bb3ab33fe7866d5c2553f
cobmin <jacob-huber@outlook.com>
2023-05-07 13:22:34 -0500

    rounding corners in posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,7 @@ export default function Index({ allPosts }: Props) {
     <>
       <Layout>
         <Head>
-          <title>{`stuff`}</title>
+          <title>{`cobmin's stuff`}</title>
         </Head>
         <Container>
           <Intro />
